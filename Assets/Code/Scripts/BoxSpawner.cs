2a87623f25627a959357b2c9cba130e23b0e8ef5
Kira <kafountain5@gmail.com>
2025-01-11 09:26:22 -0500

    more updates to box view

--- Assets/Code/Scripts/BoxSpawner.cs
+++ Assets/Code/Scripts/BoxSpawner.cs
@@ -1,9 +1,17 @@
+
+using System;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BoxSpawner : MonoBehaviour
 {
-    
     [SerializeField] private GameObject boxObject;
+    
+    // UI components
+    [SerializeField] private GameObject ui;
+    private Image image;
+    private TextMeshProUGUI text;
     private Box box;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -11,6 +19,18 @@ public class BoxSpawner : MonoBehaviour
     {
         boxObject = Instantiate(boxObject, transform);
         box = boxObject.GetComponent<Box>();
+        image = ui.GetComponentInChildren<Image>();
+        text = ui.GetComponentInChildren<TextMeshProUGUI>();
+        
+        // Hide the UI at first
+        ui.SetActive(false);
+        
+    }
+
+    // Hides the UI when not in the Box view
+    private void OnDisable()
+    {
+        ui.SetActive(false);
     }
 
     // Update is called once per frame
@@ -25,11 +45,24 @@ public class BoxSpawner : MonoBehaviour
                 
                 // Should play the click sound here 
                 
+                // Should show the popup UI here, use DotWeen to animate it
+                // Add to stack on click, coroutine that handles the popups
+                // See Red Dead for inspo
+                
+                // set UI image
+                //image.sprite = 
+                
+                // set UI text
+                text.text = output;
+                // call function to add to pile
+                
+                ui.SetActive(true);
                 
                 if (!box.NextExists())
                 {
                     // Hide the box
                     boxObject.SetActive(false);
+                    ui.SetActive(false);
                     box.Reset();
                 }
             }
