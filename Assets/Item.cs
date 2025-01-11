0a19d82874092b9d502efe0f15bb84a61aa16a0d
Zaid Minhas <61346884+ZaidMinhas@users.noreply.github.com>
2025-01-10 20:50:19 -0500

    Test

---
+++ Assets/Item.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField] string itemName;
+    [SerializeField] Color itemColor;
+    [SerializeField] string itemDescription;
+}
